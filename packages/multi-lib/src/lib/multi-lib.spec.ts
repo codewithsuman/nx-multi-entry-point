ac678721c12cc8a52edc42561738c86d92b5b8c9
Suman Jha <suman.jha@fiserv.com>
2023-01-28 18:53:48 -0500

    Generate a test lib

---
+++ packages/multi-lib/src/lib/multi-lib.spec.ts
@@ -0,0 +1,7 @@
+import { multiLib } from './multi-lib';
+
+describe('multiLib', () => {
+  it('should work', () => {
+    expect(multiLib()).toEqual('multi-lib');
+  });
+});
---
+++ packages/multi-lib/src/lib/multi-lib.ts
@@ -0,0 +1,3 @@
+export function multiLib(): string {
+  return 'multi-lib';
+}
